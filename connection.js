97e5cdf87d804624f3121bb649237974d58fd613
laurence <kkupop3@yahoo.com>
2023-02-12 03:17:32 +0100

    mz

--- connection.js
+++ connection.js
@@ -5,26 +5,22 @@ require("dotenv").config()
 
 
 const client = new Client({
-
-  /*
-  host:"containers-us-west-190.railway.app",
-  user:"postgres",
-  port:7940,
-  password: BdewPSrJ1bodLjjrwybP,
-  database:railway,
-  */
+  
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   port: process.env.PGPORT,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-//connectionString: `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`
-connectionString:"postgresql://postgres:BdewPSrJ1bodLjjrwybP@containers-us-west-190.railway.app:7940/railway"
- /*host:"localhost",
-user: "kkupop3",
-port: 5432,
-// password: "3MuLIP7MZcRHM75LkQMY",
-database: "kkupop3" */
+connectionString: `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`
+
+
+/*  DATABASE_URL=postgresql://postgres:BdewPSrJ1bodLjjrwybP@containers-us-west-190.railway.app:7940/railway
+PGDATABASE=railway
+PGHOST=containers-us-west-190.railway.app
+PGPASSWORD=BdewPSrJ1bodLjjrwybP
+PGPORT=7940
+PGUSER=postgres  */
+
 })
 
 client.connect();
