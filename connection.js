eb24f3c3353aa6ef4ac72604a1aa6ba202a1e1bc
laurence <kkupop3@yahoo.com>
2023-03-10 10:23:11 +0100

    added

--- connection.js
+++ connection.js
@@ -5,19 +5,15 @@ require("dotenv").config()
 
 
 const client = new Client({
-/*
-  host:"localhost",
-  user: "kkupop3",
-  port: 5432,
-  
-  database: "kkupop3" */
-  
+
+ 
+  /*
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   port: process.env.PGPORT,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  connectionString: process.env.DATA_URL    // define this DATA_URL in the variables section on railways.app  
+  connectionString: process.env.DATA_URL    // define this DATA_URL in the variables section on railways.app   */
   
 
 })
